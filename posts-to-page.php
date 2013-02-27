85e1c974866413b68f1334f45c24036e84916594
Kerry James <operations@bloafer.com>
2013-02-27 14:15:30 +0000

    Update to version 1.5, Fixes issues #2, #3, #4, #5

--- posts-to-page.php
+++ posts-to-page.php
@@ -2,11 +2,15 @@
 /*
 Plugin Name: Posts to Page
 Plugin URI: http://studio.bloafer.com/wordpress-plugins/posts-to-page/
-Description: Posts to page, shortcode [posts-to-page]. Usage [posts-to-page cat_ID=1]
-Version: 1.4
+Description: Posts to page, shortcode [posts-to-page]. Usage [posts-to-page cat_id=1]
+Version: 1.5
 Author: Kerry James
 Author URI: http://studio.bloafer.com/
 */
+if (!function_exists('add_action')){
+    die('You are trying to access this file in a manner not allowed.');
+}
+
 
 function post_to_page_shortcode_handler( $args, $content = null ){
     if(is_feed()){
@@ -14,27 +18,55 @@ function post_to_page_shortcode_handler( $args, $content = null ){
     }
     /* Set Defaults */
     $boolFlags = array(
-        "show_title"        => true,
-        "show_date"         => true,
-        "show_author"       => true,
-        "show_content"      => true,
-        "link_title"        => true,
-        "split_more"        => true
+        "show_title"            => true,
+        "show_date"             => true,
+        "show_author"           => true,
+        "show_content"          => true,
+        "show_categories"       => true,
+        "show_tags"             => true,
+        "show_image"            => true,
+        "link_title"            => true,
+        "link_categories"       => true,
+        "link_tags"             => true,
+        "link_image"            => true,
+        "split_more"            => true,
     );
     $dataFlags = array(
-        "type"              => "post",
-        "split_point"       => "<!--more-->",
-        "limit"             => false,
-        "cat_id"            => 1,
-        "display_sequence"  => "title,date,author,content",
-        "class_title"       => "post-to-page-title",
-        "class_date"        => "post-to-page-date",
-        "class_author"      => "post-to-page-author",
-        "class_content"     => "post-to-page-content",
-        "tag_title"         => "h2",
-        "tag_date"          => "span",
-        "tag_author"        => "span",
-        "tag_content"       => "div",
+        "type"                  => "post",
+        "split_point"           => "<!--more-->",
+        "limit"                 => false,
+        "cat_id"                => false,
+        "category"              => false,
+        "orderby"               => false,
+        "order"                 => false,
+        "offset"                => false,
+        "image_size"            => false,
+        "suppress_filters"      => false,
+        "display_sequence"      => "title,date,author,content",
+        "class_title"           => "post-to-page-title",
+        "class_date"            => "post-to-page-date",
+        "class_author"          => "post-to-page-author",
+        "class_content"         => "post-to-page-content",
+        "class_categories"      => "post-to-page-categories",
+        "class_categories_wrap" => "post-to-page-categories-wrapper",
+        "class_tags"            => "post-to-page-tags",
+        "class_tags_wrap"       => "post-to-page-tags-wrapper",
+        "class_wrap"            => "post-to-page-wrapper",
+        "class_image"           => "post-to-page-image",
+        "tag_title"             => "h2",
+        "tag_date"              => "span",
+        "tag_author"            => "span",
+        "tag_content"           => "div",
+        "tag_categories"        => "span",
+        "tag_categories_wrap"   => false,
+        "tag_tags"              => "span",
+        "tag_tags_wrap"         => false,
+        "tag_wrap"              => false,
+        "tag_image"             => "span",
+        "sep_categories"        => false,
+        "sep_tags"              => false,
+        "pre_categories"        => "Posted in: ",
+        "pre_tags"              => "Tagged in: ",
     );
 
     $displaySequence = explode(",", $dataFlags["display_sequence"]);
@@ -56,22 +88,61 @@ function post_to_page_shortcode_handler( $args, $content = null ){
 
     foreach($dataFlags as $argumentFlag=>$value){
         if(isset($args[$argumentFlag])){
+            if(trim(strtolower($args[$argumentFlag]))=="false"){
+                $dataFlags[$argumentFlag] = false;
+            }
+        }
+        if(isset($args[$argumentFlag])){
             $dataFlags[$argumentFlag] = $args[$argumentFlag];
         }
     }
 
     /* Start processing */
-    $post_args['cat']           = is_numeric($dataFlags["cat_id"])?$dataFlags["cat_id"]:1;
+    if($dataFlags["cat_id"] || $dataFlags["category"]){
+        $categoryVar = $dataFlags["category"]?$dataFlags["category"]:$dataFlags["cat_id"];
+        if(is_numeric($categoryVar)){
+            $post_args['category']       = $categoryVar;
+        }else{
+            $categoryVar = trim($categoryVar, "/");
+            $taxonomy = "category";
+            if(strstr($categoryVar, "/")){
+                $catParts = explode("/", $categoryVar);
+                if(count($catParts)==2){
+                    $taxonomy = $catParts[0];
+                    $categoryVar = $catParts[1];
+                }
+            }
+            $post_args['tax_query'][] = array(
+                'taxonomy' => $taxonomy,
+                'field' => 'slug',
+                'terms' => $categoryVar
+            );
+        }
+    }
     $post_args['post_type']     = $postType;
 
     if($dataFlags["limit"]){
         $post_args["numberposts"] = $dataFlags["limit"];
     }
+    if($dataFlags["orderby"]){
+        $post_args["orderby"] = $dataFlags["orderby"];
+    }
+    if($dataFlags["order"]){
+        $post_args["order"] = $dataFlags["order"];
+    }
+    if($dataFlags["offset"]){
+        $post_args["offset"] = $dataFlags["offset"];
+    }
+    if($dataFlags["suppress_filters"]){
+        $post_args["suppress_filters"] = $dataFlags["suppress_filters"];
+    }
 
-    $html = "";
     $displayposts = get_posts($post_args);
 
     foreach($displayposts as $post){
+        if($dataFlags["tag_wrap"]){
+            $html .= '<' . $dataFlags["tag_wrap"] . ' class="' . $dataFlags["class_wrap"] . '">' . PHP_EOL;
+        }
         foreach($displaySequence as $displaySequenceItem){
             if($displaySequenceItem=="title"){
                 if($boolFlags["show_title"]){
@@ -79,36 +150,131 @@ function post_to_page_shortcode_handler( $args, $content = null ){
                     if($boolFlags["link_title"]){ $html .= '<a href="' . get_permalink($post->ID) . '">'; }
                     $html .= $post->post_title;
                     if($boolFlags["link_title"]){ $html .= '</a>'; }
-                    $html .= '</' . $dataFlags["tag_title"] . '>';
+                    $html .= '</' . $dataFlags["tag_title"] . '>' . PHP_EOL;
                 }
             }
             if($displaySequenceItem=="date"){
                 if($boolFlags["show_date"]){
-                    $html .= '<' . $dataFlags["tag_date"] . ' class="' . $dataFlags["class_date"] . '">' . apply_filters('get_the_date', $post->post_date) . '</' . $dataFlags["tag_date"] . '>';
+                    $html .= '<' . $dataFlags["tag_date"] . ' class="' . $dataFlags["class_date"] . '">' . apply_filters('get_the_date', $post->post_date) . '</' . $dataFlags["tag_date"] . '>' . PHP_EOL;
                 }
             }
             if($displaySequenceItem=="author"){
                 if($boolFlags["show_author"]){
-                    $html .= '<' . $dataFlags["tag_author"] . ' class="' . $dataFlags["class_author"] . '">' . get_the_author_meta( "user_nicename", $post->post_author ) . '</' . $dataFlags["tag_author"] . '>';
+                    $html .= '<' . $dataFlags["tag_author"] . ' class="' . $dataFlags["class_author"] . '">' . get_the_author_meta( "user_nicename", $post->post_author ) . '</' . $dataFlags["tag_author"] . '>' . PHP_EOL;
                 }
             }
             if($displaySequenceItem=="content"){
                 if($boolFlags["show_content"]){
-                    $html .= '<' . $dataFlags["tag_content"] . ' class="' . $dataFlags["class_content"] . '">';
+                    $html .= '<' . $dataFlags["tag_content"] . ' class="' . $dataFlags["class_content"] . '">' . PHP_EOL;
                     $originalContent = $post->post_content;
                     if($boolFlags["split_more"]){
                         if(strstr($originalContent, $dataFlags["split_point"])){
                             $parts = explode($dataFlags["split_point"], $originalContent);
-                            $html .= apply_filters('the_content', $parts[0]);
+                            $html .= $parts[0];
+                        }else{
+                            if($dataFlags["suppress_filters"]){
+                                $html .= $originalContent;
+                            }else{
+                                $html .= apply_filters('the_content', $originalContent);
+                            }
+                        }
+                    }else{
+                        if($dataFlags["suppress_filters"]){
+                            $html .= $originalContent;
                         }else{
                             $html .= apply_filters('the_content', $originalContent);
                         }
-                    }else{
-                        $html .= apply_filters('the_content', $originalContent);
                     }
-                    $html .= '</' . $dataFlags["tag_content"] . '>';
+                    $html .= '</' . $dataFlags["tag_content"] . '>' . PHP_EOL;
                 }
             }
+            if($displaySequenceItem=="categories"){
+                if($boolFlags["show_categories"]){
+                    $categories = get_the_category($post->ID);
+                    if($categories){
+                        $catCount = 0;
+                        if($dataFlags["pre_categories"]){
+                            $html .= $dataFlags["pre_categories"];
+                        }
+                        if($dataFlags["tag_categories_wrap"]){
+                            $html .= '<' . $dataFlags["tag_categories_wrap"] . ' class="' . $dataFlags["class_categories_wrap"] . '">';
+                        }
+                        foreach($categories as $category){
+                            $html .= '<' . $dataFlags["tag_categories"] . ' class="' . $dataFlags["class_categories"] . '">';
+                            if($boolFlags["link_categories"]){
+                                $html .= '<a href="' . get_category_link($category->term_id ) . '">';
+                            }
+                            $html .= apply_filters('get_the_date', $category->cat_name);
+                            if($boolFlags["link_categories"]){ $html .= '</a>'; }
+                            $html .=  '</' . $dataFlags["tag_categories"] . '>';
+                            if($dataFlags["sep_categories"]){
+                                $catCount++;
+                                if($catCount!=count($categories)){
+                                    $html .= $dataFlags["sep_categories"];
+                                }
+                            }
+                        }
+                        if($dataFlags["tag_categories_wrap"]){
+                            $html .= '</' . $dataFlags["tag_categories_wrap"] . '>';
+                        }
+                    }
+                }
+            }
+            if($displaySequenceItem=="tags"){
+                if($boolFlags["show_tags"]){
+                    $tags = get_the_tags($post->ID);
+                    if($tags){
+                        $catCount = 0;
+                        if($dataFlags["pre_tags"]){
+                            $html .= $dataFlags["pre_tags"];
+                        }
+                        if($dataFlags["tag_tags_wrap"]){
+                            $html .= '<' . $dataFlags["tag_tags_wrap"] . ' class="' . $dataFlags["class_tags_wrap"] . '">';
+                        }
+                        foreach($tags as $tag){
+                            $html .= '<' . $dataFlags["tag_tags"] . ' class="' . $dataFlags["class_tags"] . '">';
+                            if($boolFlags["link_tags"]){
+                                $html .= '<a href="' . get_tag_link($tag->term_id ) . '">';
+                            }
+                            $html .= apply_filters('get_the_date', $tag->name);
+                            if($boolFlags["link_tags"]){ $html .= '</a>'; }
+                            $html .=  '</' . $dataFlags["tag_tags"] . '>';
+                            if($dataFlags["sep_tags"]){
+                                $catCount++;
+                                if($catCount!=count($tags)){
+                                    $html .= $dataFlags["sep_tags"];
+                                }
+                            }
+                        }
+                        if($dataFlags["tag_tags_wrap"]){
+                            $html .= '</' . $dataFlags["tag_tags_wrap"] . '>';
+                        }
+                    }
+                }
+            }
+            if($displaySequenceItem=="image"){
+                if($boolFlags["show_image"]){
+                    $imageSizes = false;
+                    if($dataFlags["image_size"]){
+                        $testImageSizes = explode("x", strtolower($dataFlags["image_size"]));
+                        if(count($testImageSizes)==2){
+                            $imageSizes = $testImageSizes;
+                        }
+                    }
+                    $html .= '<' . $dataFlags["tag_image"] . ' class="' . $dataFlags["class_image"] . '">';
+                    if($boolFlags["link_image"]){ $html .= '<a href="' . get_permalink($post->ID) . '">'; }
+                    if($imageSizes){
+                        $html .= get_the_post_thumbnail($post->ID, $imageSizes);
+                    }else{
+                        $html .= get_the_post_thumbnail($post->ID);
+                    }
+                    if($boolFlags["link_image"]){ $html .= '</a>'; }
+                    $html .= '</' . $dataFlags["tag_image"] . '>' . PHP_EOL;
+                }
+            }
+        }
+        if($dataFlags["tag_wrap"]){
+            $html .= '</' . $dataFlags["tag_wrap"] . '>' . PHP_EOL;
         }
     }
     return $html;
--- readme.txt
+++ readme.txt
@@ -3,45 +3,65 @@ Contributors: Bloafer
 Plugin Name: Posts to Page
 Plugin URI: http://studio.bloafer.com/wordpress-plugins/posts-to-page/
 Description: Posts to page, shortcode [posts-to-page].
-Version: 1.4
+Version: 1.5
 Author: Kerry James
 Author URI: http://studio.bloafer.com/
 Donate link: http://studio.bloafer.com/wordpress-plugins/posts-to-page/
-Tags: CMS, posts
-Requires at least: 3.1.3
+Tags: shortcode, pages, posts, custom post types, CMS
+Requires at least: 3.0
 Tested up to: 3.5.1
-Stable tag: 3.1.3
+Stable tag: 3.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-This plugin adds a shortcode [posts-to-page] to WP allowing you to place your posts into a page.
+This plugin adds a shortcode [posts-to-page] to WP allowing you to easily add one or more posts into any page.
 
 == Description ==
-This plugin adds a shortcode [posts-to-page] to WP allowing you to place your posts into a page.
+
+This plugin adds a shortcode [posts-to-page] to WP allowing you to easily add one or more posts into any page.
+
+Supports categories, tags, custom post types, custom taxonomies and more.
 
 This plugin is perfect for those who use WordPress as a CMS.
 
-if you need help with this plugin please visit http://studio.bloafer.com/wordpress-plugins/posts-to-page/
+If you need help with this plugin please visit http://studio.bloafer.com/wordpress-plugins/posts-to-page/
+
+If you find a bug or wish to give us feedback and contribute to this plugin on its [GitHub page](https://github.com/Bloafer/Posts-To-Page)
 
 == Installation ==
 
-This section describes how to install the plugin and get it working.
+You can install from within WordPress using the Plugin/Add New feature, or if you wish to manually install:
 
-1. Upload 'posts-to-pages' to the '/wp-content/plugins/' directory
-2. Activate the plugin through the 'Plugins' menu in WordPress
-3. Place [posts-to-page] in your page to display posts, eg. [posts-to-page cat_ID=1 limit=5]
+1. Download the plugin.
+2. Upload the entire `posts-to-page` directory to your plugins folder (/wp-content/plugins/)
+3. Activate the plugin from the plugin page in your WordPress Dashboard
+4. Start embedding posts in whatever pages you like using the shortcode [posts-to-page].
+
+== Common Usage ==
+
+Show top 10 posts in "Uncategorized" category, without images and comma seperated category and tag list
+[posts-to-page limit=10 category=uncategorized tag_categories_wrap="div" sep_categories=", " tag_tags_wrap="div" sep_tags=", " show_image=false]
+
+
+Show top 10 posts with bullet pointed categories and tags
+[posts-to-page limit=10 tag_categories_wrap="ul" tag_categories="li" tag_tags_wrap="ul" tag_tags="li"]
+
+Image gallery type posts, this will produce a list of image linked posts with images sized to 300x300
+[posts-to-page show_title=false show_date=false show_author=false show_content=false show_categories=false show_tags=false image_size="300x300"]
 
 == Frequently Asked Questions ==
 
 = How do I limit the results? =
 
-You need to use the variable "limit", only available in version 0.2 and above ([posts-to-page cat_ID=1 limit=3])
+You need to use the variable "limit", only available in version 0.2 and above ([posts-to-page cat_id=1 limit=3])
 
 = I want to split the content early, how? =
 
-You need to use the variable "split_point", only available in version 0.3 and above ([posts-to-page cat_ID=1 split_point="&lt;!--split--&gt;"])
+You need to use the variable "split_point", only available in version 0.3 and above ([posts-to-page cat_id=1 split_point="&lt;!--split--&gt;"])
 
 = I want to split on the more point, how would I do this? =
 
-You need to use the variable "split_more", only available in version 1.2 and above ([posts-to-page cat_ID=1 split_more="true"])
+You need to use the variable "split_more", only available in version 1.2 and above ([posts-to-page cat_id=1 split_more="true"])
 
 = I have installed the plugin but it is only showing me the shortcode? =
 
@@ -49,37 +69,73 @@ Make sure you have activated the plugin.
 
 = How do I get rid of the titles? =
 
-You need to use the variable "show_title", this has been available since version 0.1 but not documented ([posts-to-page cat_ID=1 show_title=false])
+You need to use the variable "show_title", this has been available since version 0.1 but not documented ([posts-to-page cat_id=1 show_title=false])
 
 = How do I show the date? =
 
-You need to use the variable "show_date" ([posts-to-page cat_ID=1 show_date=true])
+You need to use the variable "show_date" ([posts-to-page cat_id=1 show_date=true])
 
 = How do I show only the titles? =
 
-You need to use a two variables, "show_title" and "show_content" ([posts-to-page cat_ID=1 show_title=true show_content=false])
+You need to use a two variables, "show_title" and "show_content" ([posts-to-page cat_id=1 show_title=true show_content=false])
 
 = How do I show the author? =
 
-You need to use the variable "show_author" ([posts-to-page cat_ID=1 show_author=true])
+You need to use the variable "show_author" ([posts-to-page cat_id=1 show_author=true])
 
 = How do I link the title to the post? =
 
-You need to use the variable "link_title" ([posts-to-page cat_ID=1 link_title=true])
+You need to use the variable "link_title" ([posts-to-page cat_id=1 link_title=true])
 
 = How do I show other post types? =
 
-You need to use the variable "type" ([posts-to-page cat_ID=1 type=gallery])
+You need to use the variable "type" ([posts-to-page cat_id=1 type=gallery])
 
 = How do I display the content before the title =
 
-You need to use the "display_sequence" variable ([posts-to-page cat_ID=1 display_sequence=title,date,author,content])
+You need to use the "display_sequence" variable ([posts-to-page cat_id=1 display_sequence=title,date,author,content])
 
 = I have read all of these, but need more help =
 
 Please visit http://studio.bloafer.com/wordpress-plugins/posts-to-page/ you can find in-depth tutorials.
 
 == Changelog ==
+
+= 1.5 =
+This update adds a massive amount of functionality to improve your Posts to Page life
+
+* The "category" variable accepts taxonomy and slug selection for example [posts-to-page category=category/uncategorized]
+* The "category" variable now accepts slugs for example [posts-to-page category=uncategorized]
+* The variable "cat_ID" has now been changed to "category" to keep in line with future development, "cat_id" and "category" are interchangable to prevent legacy problems
+* Added "show_categories" variable
+* Added "show_tags" variable
+* Added "show_image" variable
+* Added "link_categories" variable
+* Added "link_tags" variable
+* Added "link_image" variable
+* Added "category" variable
+* Added "orderby" variable
+* Added "order" variable
+* Added "offset" variable
+* Added "image_size" variable
+* Added "suppress_filters" variable
+* Added "class_categories" variable
+* Added "class_categories_wrap" variable
+* Added "class_tags" variable
+* Added "class_tags_wrap" variable
+* Added "class_wrap" variable
+* Added "class_image" variable
+* Added "tag_categories" variable
+* Added "tag_categories_wrap" variable
+* Added "tag_tags" variable
+* Added "tag_tags_wrap" variable
+* Added "tag_wrap" variable
+* Added "tag_image" variable
+* Added "sep_categories" variable
+* Added "sep_tags" variable
+* Added "pre_categories" variable
+* Added "pre_tags" variable
+
 = 1.4 =
 * A long due overhaul to the post-to-page plugin
 * Changed all true/false flags to be set to true
@@ -129,8 +185,8 @@ Please visit http://studio.bloafer.com/wordpress-plugins/posts-to-page/ you can 
 
 = 0.3 =
 * Added split_point variable.
-* cat_ID now defaults to "1" if not specified.
-* cat_ID is now validated.
+* cat_id now defaults to "1" if not specified.
+* cat_id is now validated.
 * FAQ's updated.
 
 = 0.2 =
